7726d0ec44a58e3889804eee2a43257ef65f0e35
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2021-10-15 17:31:59 +0200

    no, non era un bug. sostanzialmente, quando abbiamo migrato su docker, ho sbagliato a scrivere il ternario per il sameSite e quando il sito andava in produzione mettevo i cookie con dominio localhost... boh ok grande grande

--- lib/routes/auth.ts
+++ lib/routes/auth.ts
@@ -118,7 +118,7 @@ router.post('/login|signin', utils.requiredParameters("POST", ["email", "passwor
             maxAge: parseInt(
                 process.env.REFRESH_TOKEN_TIMEOUT_MILLISECONDS as string
             ),
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'none',
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production' ? true : false
         });
